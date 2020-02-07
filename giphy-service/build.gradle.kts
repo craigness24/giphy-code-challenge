6a8d50222b118b5e5bb22b2906c6a51d116ddffd
craig <craigness24@gmail.com>
2020-02-07 16:59:22 -0600

    mongo runtime, sample rest controller using giphy api key

--- giphy-service/build.gradle.kts
+++ giphy-service/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.h2database:h2")
+	runtimeOnly("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
--- giphy-service/src/main/kotlin/com/craig/challenge/giphy/GiphyServiceApplication.kt
+++ giphy-service/src/main/kotlin/com/craig/challenge/giphy/GiphyServiceApplication.kt
@@ -1,11 +1,48 @@
 package com.craig.challenge.giphy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-class GiphyServiceApplication
+class GiphyServiceApplication {
+    @Bean
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
+        return builder.build()
+    }
+}
+
 
 fun main(args: Array<String>) {
-	runApplication<GiphyServiceApplication>(*args)
+    runApplication<GiphyServiceApplication>(*args)
+}
+
+@RestController
+class RestController(private val giphyRestClient: GiphyRestClient) {
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun get(): String? {
+        return giphyRestClient.search()
+    }
+}
+
+@Component
+class GiphyRestClient(private val props: GiphyApiProperties,
+                      private val restTemplate: RestTemplate) {
+    fun search(): String? {
+        return restTemplate.getForObject("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=${props.key}&limit=5", String::class.java)
+    }
+}
+
+@Configuration
+@ConfigurationProperties(prefix = "gipy.api")
+class GiphyApiProperties() {
+    lateinit var key: String;
 }
