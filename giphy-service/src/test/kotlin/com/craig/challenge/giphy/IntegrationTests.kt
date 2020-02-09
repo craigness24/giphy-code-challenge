eff30af3868cb1d0d1def6eb91a51a09b243662b
craig <craigness24@gmail.com>
2020-02-08 23:52:25 -0600

    integration tests

---
+++ giphy-service/src/test/kotlin/com/craig/challenge/giphy/IntegrationTests.kt
@@ -0,0 +1,30 @@
+package com.craig.challenge.giphy
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+    @MockBean
+    lateinit var giphyRestClient: GiphyRestClient
+
+    @Test
+    fun `Assert public search status is 200`() {
+        val expected = "{ sample: 5 }"
+        Mockito.`when`(giphyRestClient.search("test"))
+                .thenReturn(expected)
+
+        val entity = restTemplate.getForEntity<String>("/test")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains(expected)
+    }
+
+}
